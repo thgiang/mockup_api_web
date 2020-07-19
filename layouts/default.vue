a771d0ef2d5875c4ec9b4768a197a4c93bb1bb02
GiangTH <giangth@ghtk.vn>
2020-07-19 11:56:05 +0700

    Login template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,30 @@
 <template>
   <div>
-    <Nuxt />
+    <div class="header">
+      <div class="container-fluid">
+        <nuxt-link to="/">
+          <Logo/>
+        </nuxt-link>
+      </div>
+    </div>
+    <Nuxt/>
+    <div class="footer text-center p-2">
+      <div class="container-fluid">
+        ©2020 - GiangTH - All right reserved
+      </div>
+    </div>
   </div>
 </template>
+<script>
+</script>
+<style scoped>
+  .header {
+    background-color: #24292e;
+    height: 50px;
+  }
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .header .logo {
+    width: 50px;
+    fill: #FFF;
+  }
 </style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="login-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7">
+          <h1>Draft API tool</h1>
+          <p>MockAPI is a free tool that helps you create draft API. We have flexible Validator and smart Generator that help you create API very quickly. Response data look like real ;)<br><br>
+            When do we need to use MockAPI?<br>
+            When you want to create a demo API set before coding it in practice. The mobile team can call this draft API during the Back-end team writing actual
+            API.</p>
+        </div>
+        <div class="col-sm-5">
+          <form class="login-form p-4 bg-white text-dark">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                     placeholder="Enter email">
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-success col-12 mb-2">Login</button>
+            <nuxt-link to="/">Create a new account</nuxt-link>
+            <nuxt-link to="/" class="float-right">Forgot password?</nuxt-link>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
+<style scoped>
+  .login-section {
+    color: hsla(0, 0%, 100%, 0.6);
+    background: #2b3137 url('~assets/images/simple-codelines.svg') center 10%;
+    background-size: cover;
+    padding: 120px 0;
+  }
+
+  h1 {
+    font-size: 50px;
+    color: #FFF;
+  }
+
+  .login-form {
+    border-radius: 5px;
+  }
+</style>
